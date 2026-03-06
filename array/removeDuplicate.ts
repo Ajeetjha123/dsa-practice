3b102ff54e94d2fb9de33fb079e3a2029fed1c58
Ajeet Kumar Jha <kumarjhaajeet653@gmail.com>
2026-03-06 13:01:50 +0530

    initial commit

---
+++ array/removeDuplicate.ts
@@ -0,0 +1,12 @@
+function removeDuplicates(nums: number[]): number {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+}
+const ans = removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+console.log(ans);
